825af34d086dedcf6f3e23e5df92e184c89fb6cd
Simone Asnake <134941433+erisimasol@users.noreply.github.com>
2025-11-22 17:04:00 +0300

    role

---
+++ config/roles.js
@@ -0,0 +1,8 @@
+const ROLES = {
+    CUSTOMER: "Customer",
+    CATERING_MANAGER: "Catering Manager",
+    EXECUTIVE_CHEF: "Executive Chef",
+    SYSTEM_ADMIN: "System Admin",
+};
+
+export default ROLES;
